2831443b53e1c12a86c9818eec17c63245d0183d
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-02-05 21:41:24 +0530

    Arrays

---
+++ LeetCode/Arrays/Container With Most Water.py
@@ -0,0 +1,18 @@
+#Container With Most Water
+
+class Solution(object):
+    def maxArea(self, height):
+        left, right = 0 , len(height) - 1
+        max_area = 0
+
+        while left < right:
+
+            area =  min(height[left], height[right]) * (right - left)
+            max_area = max(area, max_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
