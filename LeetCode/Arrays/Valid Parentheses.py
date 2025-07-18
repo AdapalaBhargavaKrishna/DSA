ffbf893c5aa473872bc5529e1a335be3e1afbae1
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-07-18 22:36:28 +0530

    Valid Parentheses

---
+++ LeetCode/Arrays/Valid Parentheses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']' : '['}
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return not stack
+
+# Example 1:
+# Input: s = "()"
+# Output: true
+
+# Example 2:
+# Input: s = "()[]{}"
+# Output: true
+
+# Example 3:
+# Input: s = "(]"
+# Output: false
+
+# Example 4:
+# Input: s = "([])"
+# Output: true
