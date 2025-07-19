017b7189980a86ffa456e3ef8aae6afac4a3c706
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-07-19 13:29:21 +0530

    Binary Search

---
+++ LeetCode/Binary Search/Binary Search.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] < target:
+                low = mid + 1
+            
+            elif nums[mid] > target:
+                high = mid - 1
+
+        return -1
+
+# Example 1:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+
+# Example 2:
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
