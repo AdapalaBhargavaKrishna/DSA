4b822474ab9ab425f1bb71bd6047076c20745524
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-08-05 20:58:24 +0530

    Next Permutation

---
+++ LeetCode/Arrays/Next Permutation.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[ i + 1 ]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i] , nums[j] = nums[j] , nums[i]
+
+        left , right = i + 1, len(nums) - 1
+
+        while left < right:
+            nums[left] , nums[right] = nums[right] , nums[left]
+            left += 1
+            right -= 1
+
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: [1,3,2]
+
+# Example 2:
+# Input: nums = [3,2,1]
+# Output: [1,2,3]
+
+# Example 3:
+# Input: nums = [1,1,5]
+# Output: [1,5,1]
