de69df311ae482d83f08204622f8b93d57f64468
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-08-12 21:16:30 +0530

    Pow(x, n)

---
+++ LeetCode/Arrays/Pow(x, n).py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n == 0:
+            return 1.0
+
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        result = 1.0
+
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            x *= x 
+            n //= 2
+
+
+        return result
+    
+
+# Example 1:
+# Input: x = 2.00000, n = 10
+# Output: 1024.00000
+
+# Example 2:
+# Input: x = 2.10000, n = 3
+# Output: 9.26100
