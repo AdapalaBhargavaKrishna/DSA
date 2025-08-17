f7dd2ad160220a9928945dccb94906d37d64d7e8
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-08-17 11:21:10 +0530

    Longest Substring Without Repeating Characters

--- LeetCode/Sliding Window/Longest Substring Without Repeating Characters.py
+++ LeetCode/Sliding Window/Longest Substring Without Repeating Characters.py
@@ -15,3 +15,13 @@ class Solution(object):
             max_length = max(max_length, right - left + 1)
 
         return max_length
+
+# Example 1:
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# Example 2:
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
