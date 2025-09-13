2342c3f3b0ffd3628f13b6ee3c0a0458bab33ea9
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-09-13 21:54:44 +0530

    Rotate a LinkedList

---
+++ LeetCode/Linked List/Rotate a LinkedList.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def rotateRight(self, head, k):
+        
+        if not head or not head.next or k == 0:
+            return head
+
+        length = 1
+        current = head
+
+        while current.next:
+            current = current.next
+            length += 1
+
+        current.next = head
+
+        k  = k % length
+        steps_to_new_head = length - k
+        new_tail = head
+
+        for _ in range(steps_to_new_head - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+        
+        return new_head
