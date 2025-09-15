d6eb89de6c746e069189dae55182ac5ac4d34430
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-09-15 22:19:30 +0530

    Implement min stack

---
+++ LeetCode/Stack/Implement min stack.py
@@ -0,0 +1,40 @@
+#min stack
+class MinStack():
+
+    def __init__(self):
+        self.stack=[]
+
+
+    def push(self, val):
+        if self.isEmpty():
+            self.stack.append((val,val))
+        else:
+            self.stack.append((val,min(val,self.stack[-1][1])))
+
+    def pop(self):
+        return self.stack.pop()[0]
+
+    def top(self):
+        return self.stack[-1][0]
+
+    def getMin(self):
+        return self.stack[-1][1]
+
+    def isEmpty(self):
+        return len(self.stack)==0
+
+    def display(self):
+      print("current stack:")
+      for i in self.stack:
+        print(i[0],end = " , ")
+      print()
+
+
+s1=MinStack()
+s1.push(12)
+s1.push(15)
+s1.push(10)
+print(s1.getMin())
+print(s1.pop())
+print(s1.getMin())
+s1.display()
