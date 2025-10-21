635a38e951d5e88cd0ddfe6bec41ac80433a4285
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-10-21 21:48:46 +0530

    Subsets

--- LeetCode/Arrays/Subsets.py
+++ LeetCode/Arrays/Subsets.py
@@ -7,6 +7,21 @@ class Solution(object):
             result += [curr + [num] for curr in result]
         return result
     
+class Solution(object):
+    def subsets(self, nums):
+        result = []
+
+        def backtrack(idx , path):
+            result.append(path[:])
+            for i in range(idx ,len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+    
+        return result
+    
 # Example 1:
 # Input: nums = [1,2,3]
 # Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
