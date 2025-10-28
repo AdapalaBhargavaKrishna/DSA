02ecc49ab4e9f9642dd26e06c4bf18492a296249
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-10-28 21:46:57 +0530

    Rotting Oranges

--- LeetCode/Graphs/Rotting Oranges.py
+++ LeetCode/Graphs/Rotting Oranges.py
@@ -1,33 +1,30 @@
 def orangesRotting(grid):
     m, n = len(grid), len(grid[0])
-    queue = []
-    fresh = 0
-    time = 0
+    q, fresh = [], 0
 
     for r in range(m):
         for c in range(n):
             if grid[r][c] == 2:
-                queue.append((r, c))
+                q.append((r, c))
             elif grid[r][c] == 1:
                 fresh += 1
 
     if fresh == 0:
         return 0
 
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    time = 0
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     head = 0
 
-    while head < len(queue) and fresh > 0:
-        level_size = len(queue) - head
-        for _ in range(level_size):
-            r, c = queue[head]
-            head += 1
-            for dr, dc in directions:
+    while head < len(q) and fresh:
+        for _ in range(len(q) - head):
+            r, c = q[head]; head += 1
+            for dr, dc in dirs:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
                     grid[nr][nc] = 2
                     fresh -= 1
-                    queue.append((nr, nc))
+                    q.append((nr, nc))
         time += 1
 
     return time if fresh == 0 else -1
