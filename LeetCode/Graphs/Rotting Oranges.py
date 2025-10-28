0265015d3dd01c02f36b32f6b2a4494ce14a1140
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2025-10-28 21:44:11 +0530

    Rotting Oranges

--- LeetCode/Graphs/Rotting Oranges.py
+++ LeetCode/Graphs/Rotting Oranges.py
@@ -1,36 +1,3 @@
-from collections import deque
-
-def orangesRotting(grid):
-    q = deque()
-    time, fresh = 0, 0
-    m, n = len(grid), len(grid[0])
-
-    for r in range(m):
-        for c in range(n):
-            if grid[r][c] == 1:
-                fresh += 1
-            elif grid[r][c] == 2:
-                q.append([r,c])
-    
-    directions = [(1,0),(-1,0),(0,1),(0,-1)]
-
-    while q and fresh > 0:
-        for _ in range(len(q)):
-            r, c = q.popleft()
-            for dr, dc in directions:
-                row, col = r + dr, c + dc
-                if (row < 0 or row == m or col < 0 or col == n or grid[row][col] != 1):
-                    continue
-                grid[row][col] = 2
-                q.append([row, col])
-                fresh -= 1
-        time += 1
-    return time if fresh == 0 else -1
-
-print(orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))  # 4
-print(orangesRotting([[2,1,1],[0,1,1],[1,0,1]]))  # -1
-print(orangesRotting([[0,2]]))                    # 0
-
 def orangesRotting(grid):
     m, n = len(grid), len(grid[0])
     queue = []
@@ -64,3 +31,7 @@ def orangesRotting(grid):
         time += 1
 
     return time if fresh == 0 else -1
+
+print(orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))  # 4
+print(orangesRotting([[2,1,1],[0,1,1],[1,0,1]]))  # -1
+print(orangesRotting([[0,2]]))                    # 0
