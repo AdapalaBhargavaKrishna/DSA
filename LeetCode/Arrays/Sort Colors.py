0baf277ee087586038bec43e13ccf604dfc22040
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-01-29 21:51:16 +0530

    Sort Colors

--- LeetCode/Arrays/Sort Colors.py
+++ LeetCode/Arrays/Sort Colors.py
@@ -1,10 +1,21 @@
 class Solution(object):
     def sortColors(self, nums):
-        for i in range((len(nums))):
-            for j in range(i + 1, len(nums)):
-                if nums[i] > nums[j]:
-                    nums[i] , nums[j] = nums[j], nums[i]
-        return nums
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
     
 # Example 1:
 # Input: nums = [2,0,2,1,1,0]
