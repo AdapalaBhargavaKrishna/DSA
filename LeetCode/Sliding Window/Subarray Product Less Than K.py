043a5c2a7289ec5365eb6bd65551188a291438aa
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-02-01 20:33:20 +0530

    Subarray Product Less Than K

--- LeetCode/Sliding Window/Subarray Product Less Than K.py
+++ LeetCode/Sliding Window/Subarray Product Less Than K.py
@@ -1,9 +1,30 @@
-# Subarray Product Less Than K
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        if k <= 1:
+            return 0
 
-nums = [10, 5, 2, 6]
-k = 100
-result = []
+        left = 0
+        prod = 1
+        count = 0
 
+        for right in range(len(nums)):
+            
+            prod *= nums[right]
 
-result.append(nums[0:2])
-print(result)
+            while prod >= k:
+                prod //= nums[left]
+                left += 1
+            count += right - left + 1
+
+        return count
+
+# Example 1:
+# Input: nums = [10,5,2,6], k = 100
+# Output: 8
+# Explanation: The 8 subarrays that have product less than 100 are:
+# [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+# Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+# Example 2:
+# Input: nums = [1,2,3], k = 0
+# Output: 0
