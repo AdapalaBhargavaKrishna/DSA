04aca8fec7fd92c8f765614b9c9a6f4a0cb61c91
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-02-02 20:40:45 +0530

    Longest Repeating Character Replacement

---
+++ LeetCode/Sliding Window/Longest Repeating Character Replacement.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        left = 0
+        freq = {}
+        max_freq = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
+            max_freq = max(max_freq, freq[s[right]])
+
+            while (right - left + 1) - max_freq > k:
+                freq[s[left]] -= 1
+                left += 1
+  
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
+# Example 1:
+# Input: s = "ABAB", k = 2
+# Output: 4
+# Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+# Example 2:
+# Input: s = "AABABBA", k = 1
+# Output: 4
+# Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+# The substring "BBBB" has the longest repeating letters, which is 4.
+# There may exists other ways to achieve this answer too.
