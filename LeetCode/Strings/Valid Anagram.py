6e69167a7d6ffe6a21c4fa7677ae4320026ba9b6
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-02-11 21:03:00 +0530

    Valid Anagram

---
+++ LeetCode/Strings/Valid Anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+
+        return True
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
