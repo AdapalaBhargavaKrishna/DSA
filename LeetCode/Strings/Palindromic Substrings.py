2d5871da9aa60f5c95f43a80ca6b7d509f27ad7e
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-02-13 21:49:55 +0530

    Palindromic Substrings

--- LeetCode/Strings/Palindromic Substrings.py
+++ LeetCode/Strings/Palindromic Substrings.py
@@ -18,3 +18,13 @@ class Solution(object):
                 right += 1
         
         return count
+
+# Example 1:
+# Input: s = "abc"
+# Output: 3
+# Explanation: Three palindromic strings: "a", "b", "c".
+
+# Example 2:
+# Input: s = "aaa"
+# Output: 6
+# Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
