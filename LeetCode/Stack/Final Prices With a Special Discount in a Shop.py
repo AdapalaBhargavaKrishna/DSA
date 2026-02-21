fd6f062f5a5ddddee65207d091969851fe7ba483
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-02-21 22:32:15 +0530

    Final Prices With a Special Discount in a Shop

--- LeetCode/Stack/Final Prices With a Special Discount in a Shop.py
+++ LeetCode/Stack/Final Prices With a Special Discount in a Shop.py
@@ -1,8 +1,7 @@
 class Solution:
     def finalPrices(self, prices):
-        stack = []  # will store indices
-
-        for i in range(len(prices)):x
+        stack = []
+        for i in range(len(prices)):
             while stack and prices[stack[-1]] >= prices[i]:
                 idx = stack.pop()
                 prices[idx] -= prices[i]
