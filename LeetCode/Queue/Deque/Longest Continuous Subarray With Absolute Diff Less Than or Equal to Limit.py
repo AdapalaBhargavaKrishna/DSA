6f8f7ccb447fde9c82e4c60b46206e541cfe6779
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-06 17:06:32 +0530

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- LeetCode/Queue/Deque/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ LeetCode/Queue/Deque/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -1,28 +1,16 @@
 from collections import deque
 class Solution:
-    def longestSubarray(self, nums: List[int], limit: int) -> int:
-        
-        maxD = deque()
-        minD = deque()
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        q = deque()
 
-        left = 0
-        res = 0
-
-        for right in range(len(nums)):
-            while maxD and nums[maxD[-1]] < nums[right]:
-                maxD.pop()
-            maxD.append(right)
-
-            while minD and nums[minD[-1]] > nums[right]:
-                minD.pop()
-            minD.append(right)
-
-            while nums[maxD[0]] - nums[minD[0]] > limit:
-                if maxD[0] == left:
-                    maxD.popleft()
-                if minD[0] == left:
-                    minD.popleft()
-                left += 1
-            res = max(res, right - left + 1)
-
+        for i , num in enumerate(nums):
+            
+            while q and q[0] < i - k + 1:
+                q.popleft()
+            while q and nums[q[-1]] < num:
+                q.pop()
+            q.append(i)
+            if i >= k - 1:
+                res.append(nums[q[0]])
         return res
