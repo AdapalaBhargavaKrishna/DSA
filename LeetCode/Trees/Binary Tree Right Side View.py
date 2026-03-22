568d01b26a1363c49f6a3026bbdd9f8aab911e23
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-22 17:36:48 +0530

    Binary Tree Right Side View

--- LeetCode/Trees/Binary Tree Right Side View.py
+++ LeetCode/Trees/Binary Tree Right Side View.py
@@ -1,40 +1,31 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        def bfs(root):
+            if not root:
+                return []
 
-def rightSideView(root):
-    if not root:
-        return []
-    
-    q , res = [root] , []
+            q = deque([root])
+            result = []
 
-    while q:
-        level_size = len(q)
-        for i in range(level_size):
-            node = q.pop(0)
-            if i == level_size - 1:
-                res.append(node.val)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-    
-    return res
+            while q:
+                level_size = len(q)
+                level = []
 
-root1 = TreeNode(1)
-root1.left = TreeNode(2, None, TreeNode(5))
-root1.right = TreeNode(3, None, TreeNode(4))
-print(rightSideView(root1))  # [1, 3, 4]
-
-root2 = TreeNode(1)
-root2.left = TreeNode(2, TreeNode(4, TreeNode(5)))
-root2.right = TreeNode(3)
-print(rightSideView(root2))  # [1, 3, 4, 5]
-
-root3 = TreeNode(1, None, TreeNode(3))
-print(rightSideView(root3))  # [1, 3]
-
-root4 = None
-print(rightSideView(root4))  # []
+                for _ in range(level_size):
+                    node = q.popleft()
+                    level.append(node.val)
+                
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+                result.append(level[level_size - 1])
+            return result
+        return bfs(root)
