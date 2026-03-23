c9ec9a8d952abb2068a70fbcd9ade1e0ab3de2dd
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-23 21:19:52 +0530

    K Closest Points to Origin

--- LeetCode/Heapq/K Closest Points to Origin.py
+++ LeetCode/Heapq/K Closest Points to Origin.py
@@ -1,10 +1,16 @@
 import heapq
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
         heap = []
 
         for x , y in points:
-            dist = x**2 + y**2 
-            heapq.heappush(heap, (dist, x, y))
 
-        return [[x,y] for dist,x,y in heapq.nsmallest(k, heap)]
+            dist = x**2 + y**2 
+            heapq.heappush(heap, (-dist, x, y))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+            
+        return [[x,y] for dist,x,y in heap]
