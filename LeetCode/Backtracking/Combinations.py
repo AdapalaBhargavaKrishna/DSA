489114cd5b5c6aea5ecf628cede9590b62ac3f74
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-28 21:11:28 +0530

    Combinations

---
+++ LeetCode/Backtracking/Combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start,current):
+            if len(current) == k:
+                res.append(current[:])
+                return
+
+            for i in range(start , n + 1):
+                if i + 1 in current:
+                    continue
+                current.append(i)
+                backtrack(i + 1, current)
+                current.pop()
+        backtrack(1,[])
+        return res
