c37470ef4e14b278e1d0eb175c2cdd263089fe89
Bhargava <bk.adapala@gmail.com>
2026-04-04 21:06:56 +0530

    Divisor Game

---
+++ LeetCode/Dynamic Programming/Divisor Game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        n = len(nums)
+        dp = [False] * (n + 1)
+        dp[1] = False
+
+        for i in range(2 , n + 1):
+            for x in range(1, i):
+                if i % x == 0 and not dp[i - x]:
+                    dp[i] == True
+                    break
+        return dp[n]
